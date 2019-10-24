1ae7e8179ea7c89de0f981dd03e026595cbfbe83
小高健造 <kotaka_kenzo@f-i-d.jp>
2019-10-24 12:33:41 +0900

    add config

--- configs/app_settings.php
+++ configs/app_settings.php
@@ -2,6 +2,14 @@
 
 require_once 'configs/smarty_settings.php';
 require_once 'configs/db_connector.class.php';
+
 //ユーザー独自定義の関数
-require_once 'C:\\work\\php-apps\\smarty-3.1.33\\libs\\plugins\\myfunctions.php';
+// Windows
+//require_once 'C:\\work\\php-apps\\smarty-3.1.33\\libs\\plugins\\myfunctions.php';
+
+// Linux
+//require_once '/Users/fid/work/devel/smarty-3.1.33/libs/plugins/myfunctions.php';
+
+// PHP
 date_default_timezone_set('Asia/Tokyo');
+
--- configs/smarty_settings.php
+++ configs/smarty_settings.php
@@ -1,6 +1,8 @@
 <?php
 
-require '../smarty-3.1.33/libs/Smarty.class.php';
+//require '../smarty-3.1.33/libs/Smarty.class.php';
+require_once '/Users/fid/work/devel/smarty-3.1.33/libs/Smarty.class.php';
+
 $smarty = new Smarty;
 $smarty->debugging = false;//true:debug
 $smarty->caching = false;
